fb6f653be898afb12c8ea535bbad5d0bcdf51724
Andrey Kandybo <87312623+andreywhite01@users.noreply.github.com>
2021-09-05 12:22:31 +0700

    Add files via upload

---
+++ mass_of_blocks.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <vector>
+#include <cstdint>
+#include <set>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "russian");
+    uint32_t N = 0;
+    uint16_t R = 0;
+
+    cin >> N >> R;
+    uint64_t total_mass = 0;
+    for (uint32_t i = 0; i < N; ++i)
+    {
+        uint64_t W = 0, H = 0, D = 0;
+        cin >> W >> H >> D;
+        total_mass += W * H * D;
+    }
+    cout << total_mass * R << endl;
+    return 0;
+}
